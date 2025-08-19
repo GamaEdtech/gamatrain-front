45fe5c661b64a0338f995ae3de18bc0a812dc325
Matin jahi <matin13822332@gmail.com>
2025-08-19 18:56:59 +0330

    refactor: remove CKEditor plugin causing module conflicts
    - Removed plugins/ckeditor.js that was registering CKEditor globally
    - This was causing duplicated module errors with npm package imports
    - CKEditor is now handled directly in components via npm imports

--- plugins/ckeditor.js
+++
@@ -1,6 +0,0 @@
-import { CkeditorPlugin } from '@ckeditor/ckeditor5-vue'
-import { defineNuxtPlugin } from '#imports'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(CkeditorPlugin)
-})
