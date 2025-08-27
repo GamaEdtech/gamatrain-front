2ff78d7fcc41cab513cb18ef40ca3e9192137bb2
alireza013013 <alireza.abdollahi19998@gmail.com>
2025-08-27 12:20:20 +0330

    fix(get token holders): change load file and add types

--- server/api/get-token-holders.ts
+++ server/api/get-token-holders.ts
@@ -6,8 +6,16 @@ interface TokenHolder {
 }
 
 export default defineEventHandler(async (event) => {
-  const { Connection, PublicKey } = await import('@solana/web3.js').catch(() => {
-    throw new Error('Failed to load @solana/web3.js')
+  // const { Connection, PublicKey } = await import('@solana/web3.js').catch(() => {
+  //   throw new Error('Failed to load @solana/web3.js')
+  // })
+
+  const { PublicKey } = await import('@solana/web3.js/lib/index.esm').catch(() => {
+    throw new Error('Failed to load PublicKey @solana/web3.js')
+  })
+
+  const { Connection } = await import('@solana/web3.js').catch(() => {
+    throw new Error('Failed to load Connection @solana/web3.js')
   })
 
   const { AccountLayout } = await import('@solana/spl-token').catch(() => {
---
+++ types/solana-web3-esm.d.ts
@@ -0,0 +1,3 @@
+declare module '@solana/web3.js/lib/index.esm' {
+  export * from '@solana/web3.js'
+}
