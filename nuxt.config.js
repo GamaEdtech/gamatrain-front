622716a12862fb70d63785fd4e97a912997245d7
alireza013013 <alireza.abdollahi19998@gmail.com>
2025-09-07 13:53:37 +0330

    fix(og image): set main url in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   ],
 
   site: {
-    url: 'https://front-git-fix-seo-toturial-detail-alirezas-projects-44596e2b.vercel.app/',
+    url: 'https://gamatrain.com/',
     name: 'Gamatrain',
   },
 
