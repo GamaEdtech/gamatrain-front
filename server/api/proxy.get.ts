68222d59740df1c3100114a4d526d7fd1c1e3abd
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-11-02 18:51:35 +0330

    fix(proxy): return just token to test on vercel preview

--- server/api/proxy.get.ts
+++ server/api/proxy.get.ts
@@ -4,13 +4,13 @@ export default defineEventHandler(async () => {
   // Generate token server-side
   const token = await getVercelOidcToken()
 
-  // Call your backend API with the token
-  const res = await fetch('https://your-backend.com/secure-api', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  // // Call your backend API with the token
+  // const res = await fetch('https://your-backend.com/secure-api', {
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // })
 
   // Return only the backend response to the client
-  return Response.json(await res.json())
+  return Response.json({ token: token })
 })
