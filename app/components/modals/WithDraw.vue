a5eb1c15c7a946de788ce0f30fada78f72d1b3ae
alireza013013 <alireza.abdollahi19998@gmail.com>
2025-12-17 21:52:06 +0330

    feat(withdraw):first step withdraw complete

--- app/components/modals/WithDraw.vue
+++ app/components/modals/WithDraw.vue
@@ -27,31 +27,78 @@
         </v-col>
       </v-row>
 
-      <div class="w-100 d-flex flex-column align-center justify-center mt-4">
-        <span class="text-h5 text-center">
-          Enter the amount you want to withdraw from the account.
-        </span>
-      </div>
+      <span class="w-100 text-h5 text-start mt-4">
+        Enter the amount you want to withdraw from the account.
+      </span>
 
-      <div class="w-100 d-flex align-center justify-center mt-6">
+      <div class="w-100 d-flex flex-wrap align-start justify-start mt-6">
+        <div class="w-100 d-flex align-center justify-start mb-1">
+          <span class="text-h6 text-grey400">Your Balance :<span class="text-grey600 font-weight-bold">{{
+            Math.floor(userBalance) / 1000000
+          }}</span></span>
+        </div>
         <v-text-field
-          class="w-100 mx-4 text-center"
+          v-model="withDrawValue"
+          class="w-100"
           variant="outlined"
-          hide-details
           density="comfortable"
           rounded="lg"
           color="#ffb300"
+          :error="!!withdrawError"
+          :error-messages="withdrawError"
+          type="Number"
+          hide-spin-buttons
         >
-        <!-- <template #prepend-inner>
-          <img
-            width="20"
-            height="20"
-            :src="selectedCurrency.logoURI"
-            alt="Coin Logo"
-            class="rounded-circle"
-          >
-        </template> -->
+          <template #prepend-inner>
+            <img
+              width="20"
+              height="20"
+              src="/images/gama-coin.svg"
+              alt="Coin Logo"
+              class="rounded-circle"
+            >
+          </template>
         </v-text-field>
+        <div class="w-100 d-flex align-center justify-start ga-2 mt-4">
+          <v-btn
+            flat
+            color="info"
+            width="70"
+            max-height="26"
+            rounded="pill"
+            variant="tonal"
+            class="text-h5 font-weight-bold"
+            @click="selectMaximumBalance"
+          >
+            Max
+          </v-btn>
+          <v-btn
+            flat
+            color="info"
+            width="70"
+            max-height="26"
+            rounded="pill"
+            variant="tonal"
+            class="text-h5 font-weight-bold"
+            @click="selectHalfBalance"
+          >
+            Half
+          </v-btn>
+        </div>
+      </div>
+
+      <div class="w-100 d-flex align-center justify-center mt-4">
+        <v-btn
+          :disabled="disableFistStep"
+          color="success"
+          flat
+          rounded="lg"
+          max-width="200"
+          class="w-50 font-weight-bold text-h5 mt-4 mx-auto"
+          @click="goToSecondStep"
+        >
+          Next Step
+        </v-btn>
       </div>
     </div>
   </v-dialog>
@@ -83,16 +130,50 @@ const dialogModel = computed({
 
 const closeModal = () => {
   emit('update:showDialog', false)
+  withDrawValue.value = 0
 }
 
 const clickOnOverlay = () => {
   if (!mdAndUp.value) {
     emit('update:showDialog', false)
+    withDrawValue.value = 0
   }
 }
 
 const clickOnModal = (event: Event) => {
   event.stopPropagation()
+}
+
+const withDrawValue = ref(0)
+
+const selectMaximumBalance = () => {
+  withDrawValue.value = props.userBalance / 1_000_000
+}
+
+const selectHalfBalance = () => {
+  withDrawValue.value = (props.userBalance / 2) / 1_000_000
+}
+
+const userBalanceInCoin = computed(() => {
+  return props.userBalance / 1_000_000
+})
+const withdrawError = computed(() => {
+  if (withDrawValue.value <= 0) {
+    return 'Amount must be greater than zero'
+  }
+
+  if (withDrawValue.value > userBalanceInCoin.value) {
+    return 'Amount exceeds your balance'
+  }
+
+  return ''
+})
+const disableFistStep = computed(() => {
+  return withdrawError.value !== ''
+})
+
+const goToSecondStep = () => {
+
 }
 </script>
 
