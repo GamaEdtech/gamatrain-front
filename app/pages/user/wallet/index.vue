54abe879d5b088840bd9049160446bd1ccd0b750
alireza013013 <alireza.abdollahi19998@gmail.com>
2026-02-05 17:10:46 +0330

    fix(wallet user): refactor balance card , remove category from dashboard layout, move images to assets

--- app/pages/user/wallet/index.vue
+++ app/pages/user/wallet/index.vue
@@ -1,100 +1,26 @@
 <template>
-  <div class="wallet-page">
-    <div class="wallet-page-container">
-      <!-- Balance Card and Chart Section -->
-      <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-          md="6"
-        >
-          <user-wallet-balance-card />
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="6"
-          class="d-none d-sm-block"
-        >
-          <user-wallet-transaction-chart
-            class="transaction-chart"
-          />
-        </v-col>
-      </v-row>
+  <v-container class="w-100 d-flex flex-column">
+    <div class="wallet-chart-div bg-grey100 rounded-lg pa-2 d-flex flex-column align-center justify-center">
+      <user-wallet-balance-card />
     </div>
-    <div class="px-4">
-      <!-- Mobile Chart (Initially Hidden) -->
-      <v-expand-transition>
-        <div
-          v-if="showMobileChart"
-          class="d-block d-sm-none mt-6 mobile-chart-container"
-        >
-          <div class="pa-4 rounded-lg bg-primary-gray-100">
-            <user-wallet-transaction-chart
-              class="transaction-chart h-full"
-            />
-          </div>
-        </div>
-      </v-expand-transition>
-
-      <!-- Transaction History Section -->
-      <user-wallet-transaction-history
-        :is-chart-visible="showMobileChart"
-        @toggle-chart="toggleMobileChart"
-      />
-    </div>
-  </div>
+  </v-container>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-// Page metadata
+<script setup lang="ts">
 definePageMeta({
   layout: 'dashboard-layout',
 })
 
-// Set page title
 useHead({
   title: 'Wallet - Gama',
   meta: [
     { name: 'description', content: 'Manage your GET tokens and view transaction history' },
   ],
 })
-
-// Define reactive state
-const showMobileChart = ref(false)
-
-// Methods
-const toggleMobileChart = () => {
-  showMobileChart.value = !showMobileChart.value
-}
 </script>
 
 <style scoped>
-.wallet-page-container {
-  background: #f2f4f7 !important;
-  padding: 26px 26px;
-}
-@media only screen and (min-width: 600px) {
-  .wallet-page {
-    padding: 16px;
-  }
-  .wallet-page-container {
-    background: #f2f4f7 !important;
-    border-radius: 16px;
-    padding: 20px 20px;
-  }
-}
-
-.transaction-chart {
-  margin-bottom: 16px;
-  position: relative;
-  width: 100%;
-  height: max-content;
-}
-
-.mobile-chart-container .transaction-chart {
-  height: 250px;
+.wallet-chart-div{
+  max-width: 370px;
 }
 </style>
