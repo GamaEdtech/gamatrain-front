f8be0af1120afc00209e8a3f877cf0555874ef2e
Farimah <farimah.fattahi2000@gmail.com>
2026-06-10 14:24:03 +0330

    chore(school-card): revert to previous version

--- app/components/school/card/index.vue
+++ app/components/school/card/index.vue
@@ -1,14 +1,10 @@
 <template>
-  <div
-    class="card-wrapper"
-  >
     <div class="w-100 d-none d-md-flex justify-center">
       <school-card-desktop :school="school" />
     </div>
     <div class="w-100 d-flex justify-center d-md-none">
       <school-card-mobile :school="school" />
     </div>
-  </div>
 </template>
 
 <script setup>
@@ -20,12 +16,4 @@ defineProps({
 </script>
 
 <style scoped>
-.card-wrapper {
-  width: fit-content;
-}
-@media (min-width: 960px) {
-  .card-wrapper {
-    width: 100%;
-  }
-}
 </style>
