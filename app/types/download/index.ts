88bc1f56fd8dac62de18dce63fa2fb65b9466928
alireza013013 <alireza.abdollahi19998@gmail.com>
2026-07-18 13:50:01 +0330

    feat(download): add download functionality with API integration in composable and add types

---
+++ app/types/download/index.ts
@@ -0,0 +1,16 @@
+export type ContentTypeDownload = 'PastPaper' | 'Multimedia' | 'Exam'
+
+export interface BodyRequestDownloadDTO {
+  id: string | number
+  contentType: ContentTypeDownload
+  fileType: string
+  extraId?: string | number
+}
+
+export type PaidByDownloadResponseDTO = 'SubscriptionQuota' | 'Points'
+export interface DownloadResponseDTO {
+  url: string
+  name: string
+  spent: boolean
+  paidBy: PaidByDownloadResponseDTO
+}
