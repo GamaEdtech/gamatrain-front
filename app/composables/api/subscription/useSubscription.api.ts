1adbf8b058da7e2f613126f35f7474e258c89c14
alireza013013 <alireza.abdollahi19998@gmail.com>
2026-08-08 17:46:10 +0330

    feat(subscription): add subscription types and composable for managing subscription plans

---
+++ app/composables/api/subscription/useSubscription.api.ts
@@ -0,0 +1,37 @@
+import type {
+  ApiResult,
+  AdminSubscriptionPlanDTO,
+} from '@/types'
+
+export const useSubscription = () => {
+  const { handleApiResponseError, handleApiCatchError } = useApiErrorHandler()
+
+  const data = ref<AdminSubscriptionPlanDTO[]>([])
+  const loadingGetData = ref(true)
+
+  const getData = async () => {
+    loadingGetData.value = true
+    try {
+      const response = await useApiService.get<
+        ApiResult<AdminSubscriptionPlanDTO[]>
+      >(`/api/v2/subscriptions/plans`)
+
+      if (response.succeeded && response.data) {
+        data.value = response.data
+      }
+      else {
+        data.value = []
+        handleApiResponseError(response)
+      }
+    }
+    catch (err: unknown) {
+      data.value = []
+      handleApiCatchError(err)
+    }
+    finally {
+      loadingGetData.value = false
+    }
+  }
+
+  return { loadingGetData, data, getData }
+}
