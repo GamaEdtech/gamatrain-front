f52dc394ebaee65ba7c4bd5ee69ebd925f6bb3b4
alireza013013 <alireza.abdollahi19998@gmail.com>
2026-08-11 11:34:45 +0330

    fix(exam,multimedia): fix route.push params if content not found

--- app/pages/exam/[id]/[[slug]].vue
+++ app/pages/exam/[id]/[[slug]].vue
@@ -128,7 +128,7 @@ const { data: contentData } = await useAsyncData(
       })
       const error = e as AppError
       if (error?.status === 404) {
-        router.push('/search?type=paper')
+        router.push('/search?type=quizhub')
       }
       throw error
     }
