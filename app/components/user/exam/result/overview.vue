0407eb7e0107825d0fca10e7b6b118591265316a
alireza013013 <alireza.abdollahi19998@gmail.com>
2026-09-13 11:49:56 +0330

    refactor(exam): update user avatar handling and add result summary component

--- app/components/user/exam/result/overview.vue
+++ app/components/user/exam/result/overview.vue
@@ -39,9 +39,9 @@
           color="grey100"
         >
           <v-img
-            v-if="contentData?.user.avatar"
-            :src="contentData?.user.avatar"
-            :alt="participantName"
+            v-if="user?.avatarUri"
+            :src="user?.avatarUri"
+            :alt="fullName"
           />
           <v-icon
             v-else
@@ -54,7 +54,7 @@
 
         <div class="min-w-0">
           <div class="text-grey800 text-h5 font-weight-bold text-truncate">
-            {{ participantName }}
+            {{ fullName }}
           </div>
           <div class="text-grey500 text-h6 mt-1">
             Code: {{ contentData?.exam.code }}
@@ -86,10 +86,13 @@ const props = defineProps<{
   contentData: ExamResultDetailDTO | null
 }>()
 
-const participantName = computed(() => {
-  const user = props.contentData?.user
-  return `${user?.first_name || ''} ${user?.last_name || ''}`.trim() || '-'
-})
+const { user } = useUser()
+const fullName = useFullName(user.value!)
+
+// const participantName = computed(() => {
+//   const user = props.contentData?.user
+//   return `${user?.first_name || ''} ${user?.last_name || ''}`.trim() || '-'
+// })
 
 const isComplete = computed(() => Number(props.contentData?.userData.status) === 1)
 
